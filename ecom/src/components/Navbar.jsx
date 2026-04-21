4f1bce31e9bc5daef8552deb3e59b7945a931ed2
mashettypavan <pavanmashetty@gmail.com>
2026-04-21 19:51:56 +0530

    added login signup navbar with routing

--- ecom/src/components/Navbar.jsx
+++ ecom/src/components/Navbar.jsx
@@ -1,26 +1,17 @@
+import { Link } from 'react-router-dom'
 import '../home.css'
 
 function Navbar() {
-
- 
-
   return (
-
-    <div>
-
-        <ul className='navbar'>
-        <li>Home</li>
-        <li>Products</li>
-        <li>Login/Register</li>
-        <li>About</li>
-        
-        </ul> 
-    
+   <div>
+      <ul className='navbar'>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/products">Products</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/about">about</Link></li>
+      </ul>
     </div>
-  
-);
+  );
 }
 
 export default Navbar;
-
-
---
+++ ecom/src/pages/login.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+import '../auth.css'
+
+function Login() {
+  return (
+    <div className="auth-container">
+      <div className="auth-card">
+        <h2>Welcome Back</h2>
+        <p className="auth-subtitle">Login to your account</p>
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" placeholder="Enter your email" />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" placeholder="Enter your password" />
+        </div>
+        <button className="auth-btn">Login</button>
+        <p className="auth-footer">Don't have an account? <Link to="/signup">Sign Up</Link></p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
